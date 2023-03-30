515eaedee0b44d6f0bc7bd645a57fafbe9c84ef7
Simrat Singh Hayer <shayer@mantech-inc.com>
2023-03-30 17:29:39 -0400

    added server which takes in 2 variable and print. added a react frontend which takes in 2 variable and print

--- Frontend/house_scrape/src/App.js
+++ Frontend/house_scrape/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// TextBoxes.js
+import React, { useState } from 'react';
 
-function App() {
+function TextBoxes({ onSubmit }) {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+
+  const handleText1Change = (event) => {
+    setText1(event.target.value);
+  }
+
+  const handleText2Change = (event) => {
+    setText2(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    //event.preventDefault();
+    onSubmit(text1, text2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={text1} onChange={handleText1Change} />
+      <input type="text" value={text2} onChange={handleText2Change} />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default App;
+export default TextBoxes;
--- Frontend/house_scrape/src/index.js
+++ Frontend/house_scrape/src/index.js
@@ -1,17 +1,15 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import TextBoxes from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+function handleSubmit(text1, text2) {
+  console.log('Text1:', text1);
+  console.log('Text2:', text2);
+}
+
+ReactDOM.render(
+  <TextBoxes onSubmit={handleSubmit} />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
