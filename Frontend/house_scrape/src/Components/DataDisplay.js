5e519069c6f80af6aa8e959006907804c2d26770
imran ali <farhandxb96@gmail.com>
2023-03-31 23:06:07 -0400

    added shadow and spacing between components

--- Frontend/house_scrape/src/Components/DataDisplay.js
+++ Frontend/house_scrape/src/Components/DataDisplay.js
@@ -6,7 +6,7 @@ function DataDisplay ({ imageUrl, price, description, url }) {
   }
 
   return (
-    <div onClick={handleClick} style={{ display: 'flex', alignItems: 'center', padding: '16px', border: '1px solid #ccc', borderRadius: '8px', cursor: 'pointer' }}>
+    <div onClick={handleClick} style={{ display: 'flex', alignItems: 'center', padding: '16px', border: '1px solid #ccc', borderRadius: '8px', cursor: 'pointer', marginBottom: '16px', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }}>
       <img src={imageUrl} alt="product" style={{ width: '100px', height: '100px', objectFit: 'cover', marginRight: '16px' }} />
       <div style={{ flex: 1 }}>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
