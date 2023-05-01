fc8adc60d7b105b11a675c01c472a28d7f82f39f
imran ali <farhandxb96@gmail.com>
2023-04-30 22:52:41 -0400

    reduce size

--- Frontend/house_scrape/src/Components/LogoIntro.js
+++ Frontend/house_scrape/src/Components/LogoIntro.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import FormLabel from '@mui/material/FormLabel';
 import './LogoIntro.css';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import AdbIcon from '@mui/icons-material/Adb';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Container } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import logoImage from './Resources/logo.png'
 
